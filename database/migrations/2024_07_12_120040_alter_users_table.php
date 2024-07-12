2ad13f354b11c1bc1ec16881954950675cf20b01
Humberto Filho <hr.soaresfilho@gmail.com>
2024-07-12 09:07:31 -0300

    Alterado a tabela Users para permitir null no campo local_id e manter 3 como default para titulo_permissao_id

---
+++ database/migrations/2024_07_12_120040_alter_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('titulo_permissao_id')->default(3)->change();
+            $table->unsignedBigInteger('local_id')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('titulo_permissao_id')->default(null)->change();
+            $table->unsignedBigInteger('local_id')->nullable(false)->change();
+        });
+    }
+}
